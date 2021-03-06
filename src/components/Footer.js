618271033c70e837c78c0f081c1de5f91a74e0c0
susheelpabbathy <70330859+susheelpabbathy@users.noreply.github.com>
2021-03-06 18:55:07 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
         <p className="copyright">
-         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="http://www.adityaketkar.me"> Aditya Ketkar</a>  
+         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="http://www.adityaketkar.me">SusheelPabbathy</a>  
         <br/>
         Get your own version of this webpage <a href="https://github.com/adityaketkar/circle-packing-personal-homepage"> here </a>
         </p>
